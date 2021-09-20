2b26559a23922a35af7875b630383d16757b1c5d
GeralPinzon <geraldinpinzonb@gmail.com>
2021-09-20 17:03:16 -0500

    Geral Pinzon: Division carpetas

--- API/Gesture.py
+++ API/Gesture.py
@@ -1,9 +1,9 @@
-from middle import validation 
+from Command.middle import validation
 from flask import Flask
 from flask import request
-from flask import jsonify
 
 app = Flask(__name__)
+
 
 @app.route('/api/gesture', methods=['POST'])
 def ejemplo5():
@@ -11,8 +11,8 @@ def ejemplo5():
         content = request.get_json(force=True)
         return validation(content)
 
+
 if __name__ == "__main__":
-    
     app.run(host='0.0.0.0',
             debug=True,
             port=8089)
